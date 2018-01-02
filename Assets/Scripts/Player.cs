e314bd4859137aaf0d860ecd105c3c9f912c4651
jamiex304 <jamiex304@hotmail.com>
2018-01-02 21:11:08 +0000

    CrossPlatformInputManager Setup
    Basic Scene Build
    Import of the CrossPlatformInput Assets
    Script for Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Simply Printing Out the Horizontal and Vertical Values from different Inputs such as WSAD and Controller (Xbox Controller Used)
+		print("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+		print("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+	}
+}
