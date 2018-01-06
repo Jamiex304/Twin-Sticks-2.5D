f6e7adaed30c47f6fb6c5cd5819f2ca802b9745e
jamiex304 <jamiex304@hotmail.com>
2018-01-06 17:59:46 +0000

    Secnd Stick Controller Setup
    Camera Follows the Player and can be rotated around as the player moves

---
+++ Assets/Scripts/CameraPan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void LateUpdate () {
+		//print("RHoriz" + Input.GetAxis("RHoriz"));
+		//print("RVert" + Input.GetAxis("RVert"));
+		transform.LookAt(player.transform);
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,12 +6,14 @@ using UnityStandardAssets.CrossPlatformInput;
 public class GameManager : MonoBehaviour {
 
 	public bool recording = true;
+	private float initialFixedTime;
 
 	void Start(){
 		//Unlocking Levels (Proof)
 		PlayerPrefsMaster.UnlockLevel(2);
 		print(PlayerPrefsMaster.IsLevelUnlocked(1));
 		print(PlayerPrefsMaster.IsLevelUnlocked(2));
+		initialFixedTime = Time.fixedDeltaTime;
 	}
 
 	// Update is called once per frame
@@ -21,6 +23,23 @@ public class GameManager : MonoBehaviour {
 		}else{
 			recording = true;
 		}
-		
+
+		if(Input.GetKeyDown(KeyCode.P)){
+			PauseGame();
+		}
+
+		if(Input.GetKeyDown(KeyCode.R)){
+			ResumeGame();
+		}
+	}
+
+	void PauseGame(){
+		Time.timeScale = 0;
+		Time.fixedDeltaTime = 0;
+	}
+
+	void ResumeGame(){
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = initialFixedTime;
 	}
 }
